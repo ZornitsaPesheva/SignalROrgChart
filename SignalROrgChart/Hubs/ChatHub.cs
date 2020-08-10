a8d79a11c2d5ed30a9b5a6d140356c9b906ca095
Zornitsa Serbezova <zornitsa.serbezova@live.com>
2020-08-10 16:31:11 +0300

    commit

---
+++ SignalROrgChart/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendData(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveData", user, message);
+        }
+    }
+}
